19c2cf52ac283083dc0c822220314d2e9ded7a57
MX-DIGITAL <emprendejosuev@gmail.com>
2025-10-28 18:13:53 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -2,74 +2,24 @@ import pkg from '@whiskeysockets/baileys'
 const {generateWAMessageFromContent, proto} = pkg
 
 var handler = async (m, {conn, usedPrefix}) => {
-let msg = generateWAMessageFromContent(
-m.chat,
-{
-viewOnceMessage: {
-message: {
-messageContextInfo: {
-deviceListMetadata: {},
-deviceListMetadataVersion: 2
-},
+let msg = generateWAMessageFromContent(m.chat, { viewOnceMessage: { message: { messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
 interactiveMessage: proto.Message.InteractiveMessage.create({
-body: proto.Message.InteractiveMessage.Body.create({
-text: 'test'
-}),
-footer: proto.Message.InteractiveMessage.Footer.create({
-text: 'test'
-}),
+body: proto.Message.InteractiveMessage.Body.create({ text: 'Prueba 1' }),
+footer: proto.Message.InteractiveMessage.Footer.create({ text: 'Prueba 2' }),
 header: proto.Message.InteractiveMessage.Header.create({
-title: 'test',
-subtitle: 'test',
-hasMediaAttachment: false
-}),
-nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+title: 'Titulo 3', subtitle: 'Sub titulado.', hasMediaAttachment: false
+}), nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
 buttons: [
-{
-name: 'single_select',
-buttonParamsJson:
-'{"title":"title","sections":[{"title":"title","highlight_label":"label","rows":[{"header":"header","title":"title","description":"description","id":"id"},{"header":"header","title":"title","description":"description","id":"id"}]}]}'
-},
-{
-name: 'quick_reply',
-buttonParamsJson: '{"display_text":"quick_reply","id":"message"}'
-},
-{
-name: 'cta_url',
-buttonParamsJson: '{"display_text":"url","url":"https://www.google.com","merchant_url":"https://www.google.com"}'
-},
-{
-name: 'cta_call',
-buttonParamsJson: '{"display_text":"call","id":"message"}'
-},
-{
-name: 'cta_copy',
-buttonParamsJson: '{"display_text":"copy","id":"123456789","copy_code":"message"}'
-},
-{
-name: 'cta_reminder',
-buttonParamsJson: '{"display_text":"cta_reminder","id":"message"}'
-},
-{
-name: 'cta_cancel_reminder',
-buttonParamsJson: '{"display_text":"cta_cancel_reminder","id":"message"}'
-},
-{
-name: 'address_message',
-buttonParamsJson: '{"display_text":"address_message","id":"message"}'
-},
-{
-name: 'send_location',
-buttonParamsJson: ''
-}
-]
-})
-})
-}
-}
-},
-{}
-)
+{ name: 'single_select', buttonParamsJson: '{"title":"Listas ⚡","sections":[{"title":"Descargas 🎄","highlight_label":"Popular","rows":[{"header":"Play","title":"Descargador 🎬","description":"Presione para seleccionar.","id":"#play"},{"header":"header","title":"title","description":"description","id":"#tiktok"}]}]}' },
+{ name: 'quick_reply', buttonParamsJson: '{"display_text":"⚡ Menu","id":"message"}' },
+{ name: 'cta_url', buttonParamsJson: '{"display_text":"ENLACE 🎲","url":"https://www.google.com","merchant_url":"https://www.google.com"}' },
+{ name: 'cta_call', buttonParamsJson: '{"display_text":"call","id":"message"}' },
+{ name: 'cta_copy', buttonParamsJson: '{"display_text":"Copiar 🎬","id":"#ig","copy_code":"message"}' }, 
+{ name: 'cta_reminder', buttonParamsJson: '{"display_text":"Seguir","id":"#play"}' },
+{ name: 'cta_cancel_reminder', buttonParamsJson: '{"display_text":"No seguir","id":"#play"}' },
+{ name: 'address_message', buttonParamsJson: '{"display_text":"Promocionar","id":"#menu"}' },
+{ name: 'send_location', buttonParamsJson: ''}
+]})})}}}, {})
 
 await conn.relayMessage(msg.key.remoteJid, msg.message, {messageId: msg.key.id})
 }
