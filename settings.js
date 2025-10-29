47c64167105a2c434146ea3bd4cdf039d13887f9
MX-DIGITAL <emprendejosuev@gmail.com>
2025-10-29 02:15:59 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,36 +1,23 @@
+
 import { watchFile, unwatchFile } from "fs"
 import chalk from "chalk"
 import { fileURLToPath } from "url"
 import fs from "fs"
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumber = "" //Ejemplo: 573218138672
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.owner = [
-// <-- Número @s.whatsapp.net -->
-["5493873655135", "🜲 Propietario", true],
-["5493873579805"]
-]
-
+global.owner = [["5493873655135", "🜲 Propietario", true], ["5493873579805"]]
 global.mods = ["5493873655135"]
 global.suittag = ["5493873655135"] 
 global.prems = ["5493873655135"]
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.libreria = "Baileys Multi Device"
-global.vs = "^1.8.2|Latest"
-global.nameqr = "MiyukiBot-MD"
+global.vs = "^6.4.6-mx"
+global.nameqr = "MX-BOT"
 global.sessions = "Sessions/Principal"
 global.jadi = "Sessions/SubBot"
 global.shadow_xyzJadibts = true
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.xMage = "https://qu.ax/BAuJt.jpg"
 global.xMage2 = "https://qu.ax/BTUHt.jpg"
@@ -40,28 +27,24 @@ global.xImagen2 = "https://qu.ax/XPDQK.jpg"
 global.xImagen3 = "https://qu.ax/GbfQk.jpg"
 
 global.botname = "MX-BOT"
-global.textbot = "Bot WhatsApp."
+global.textbot = "Designed and created by Alan.Js / @mdmx_mktg"
 global.textoInfo = "Designed and created by Alan.Js / @mdmx_mktg"
-global.dev = "Alan.Js"
-global.author = "« Alan.Js »"
-global.etiqueta = "[Alan.Js]"
+global.dev = "@mx-community"
+global.author = "@mx-community"
+global.etiqueta = "@mx-community"
 global.currency = "Monedas"
-global.currency2 = "Diamantes"
+global.currency2 = "Bateria"
 global.currency3 = "Puntos"
-global.banner = "https://qu.ax/ndWOF.jpg"
-global.icono = 'https://qu.ax/ndWOF.jpg'
+global.banner = "https://qu.ax/BAuJt.jpg"
+global.icono = 'https://qu.ax/BTUHt.jpg'
 global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.channel = "https://whatsapp.com/channel/0029Vb6wMPa8kyyTpjBG9C2H"
-global.github = "https://github.com/OmarGranda/MiyukiBot-MD" 
-global.gmail = "omargranda673@gmail.com"
+global.github = "https://github.com/mx-community/" 
+global.gmail = "emprendejosuev@gmail.com"
 global.ch = {
 ch1: "120363318353263389@newsletter"
 }
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.APIs = {
 xyro: { url: "https://xyro.site", key: null },
@@ -72,11 +55,10 @@ zenzxz: { url: "https://api.zenzxz.my.id", key: null },
 siputzx: { url: "https://api.siputzx.my.id", key: null }
 }
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
 unwatchFile(file)
 console.log(chalk.redBright("Update 'settings.js'"))
 import(`${file}?update=${Date.now()}`)
 })
+  
