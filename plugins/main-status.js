e497545edea933c6043a8192cbb8ce11c9ab5a21
MX-DIGITAL <emprendejosuev@gmail.com>
2025-10-28 21:27:56 -0300

    Update main-status.js

--- plugins/main-status.js
+++ plugins/main-status.js
@@ -9,13 +9,34 @@ let totalUsers = Object.keys(global.db.data.users).length
 let totalChats = Object.keys(global.db.data.chats).length
 let totalPlugins = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 let totalBots = global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== 3).length
-let system = `*「✦」Estado del Sistema*\n\n◇ *Comandos ejecutados* » ${toNum(totalStats)}\n◇ *Usuarios registrados* » ${totalUsers.toLocaleString()}\n◇ *Grupos registrados* » ${totalChats.toLocaleString()}\n◇ *Plugins* » ${totalPlugins}\n◇ *Bots Activos* » ${totalBots}\n\n❍ *Estado del Servidor*\n\n◆ *Sistema* » ${platform()}\n◆ *CPU* » ${_cpus().length} cores\n◆ *RAM* » ${format(totalmem())}\n◆ *RAM Usado* » ${format(totalmem() - freemem())}\n◆ *Arquitectura* » ${process.arch}\n◆ *Host ID* » ${hostname().slice(0, 8)}...\n\n*❑ Uso de Memoria NODEJS*\n\n◈ *Ram Utilizada* » ${format(process.memoryUsage().rss)}\n◈ *Heap Reservado* » ${format(process.memoryUsage().heapTotal)}\n◈ *Heap Usado* » ${format(process.memoryUsage().heapUsed)}\n◈ *Módulos Nativos* » ${format(process.memoryUsage().external)}\n◈ *Buffers de Datos* » ${format(process.memoryUsage().arrayBuffers)}`
-await conn.reply(m.chat, system, m, rcanal)
+let system = `⊸⊹ \`ESTADO : STATUS\`
+
+❒ *Comandos usados:* ${toNum(totalStats)}
+❒ *Registrados:* ${totalUsers.toLocaleString()}
+❒ *Grupos totales:* ${totalChats.toLocaleString()}
+❒ *Plugins:* ${totalPlugins}
+❒ *Servidores:* ${totalBots}
+
+❒ *Sistema:* ${platform()}
+❒ *CPU:* ${_cpus().length} cores
+❒ *RAM:* » ${format(totalmem())}
+❒ **RAM en uso:* ${format(totalmem() - freemem())}
+❒ *Arquitectura:* » ${process.arch}
+❒ *Host ID:* » ${hostname().slice(0, 8)}...
+
+*❑ Uso de Memoria NODEJS*\n\n
+❒ *Ram Utilizada:* ${format(process.memoryUsage().rss)}
+❒ *Heap Reservado:* ${format(process.memoryUsage().heapTotal)}
+❒ *Heap Usado:* ${format(process.memoryUsage().heapUsed)}
+❒ *Módulos Nativos:* ${format(process.memoryUsage().external)}
+❒ *Buffers de Datos:* ${format(process.memoryUsage().arrayBuffers)}`
+await conn.sendMessage(m.chat, { text: system, contextInfo: { externalAdReply: { title: botname, body: textoInfo, thumbnailUrl: xImagen2, sourceUrl: null, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}} , { quoted: m })
+//conn.reply(m.chat, system, m, rcanal)
 }
 
 handler.help = ['estado']
 handler.tags = ['info']
-handler.command = ['estado', 'status']
+handler.command = ['estado', 'status', 'stat']
 
 export default handler
 
@@ -27,3 +48,5 @@ return (number / 1000000).toFixed(1) + 'M'
 } else {
 return number.toString()
 }}
+
+
